a9c1b347cef0ea711b25fc7fdf79a27a49dda0c2
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-03-05 00:40:08 +0600

    crypto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 
 // -------------stream -----
@@ -15,6 +16,19 @@ const readStream = fs.createReadStream('input.txt');
 
 // -----------buffer to string
 
-readStream.on('data', (chunk) => {
-    console.log('Received chunk:', chunk. toLocaleString());
-});
+// readStream.on('data', (chunk) => {
+//     console.log('Received chunk:', chunk. toLocaleString());
+// });
+
+
+// ------------crypto thats mean password hide 
+const hash = crypto.createHash('sha256');
+
+
+// Update the hash with data
+hash.update('Hello, World!');
+
+// Get the digest in hexadecimal format
+const digest = hash.digest('hex');
+console.log(digest);
+
